5d59641012483abe36dac6d979c2ed45dec58621
KgomotsoSalman <164500988+KgomotsoSalman@users.noreply.github.com>
2024-09-15 23:35:15 +0200

    added github and netlify url

--- src/App.js
+++ src/App.js
@@ -6,7 +6,11 @@ export default function App() {
     return ( <
         div className = "App" >
         <
-        h1 > Weather App < /h1> < /
+        h1 > Weather App < /h1>  <
+        p > Created by Kgomotso Salman and is opensource on < a href = "https://github.com/KgomotsoSalman/my-react-app" > Github < /a>and hosted on <a href=" https:/ / mongas - react - app.netlify.app / ">Netlify</a></p>
+
+        <
+        /
         div >
     );
 }
